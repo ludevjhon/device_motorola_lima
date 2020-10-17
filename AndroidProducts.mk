f0ebb772a717d27f8191a1762c947f64d91cfb76
Ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-10-17 12:58:50 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,8 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
     $(LOCAL_DIR)/lineage_lima.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lima-user \
+    lineage_lima-userdebug \
+    lineage_lima-eng \
